9c6889bccbf3cb74f13808ce4bdeef945bc90929
Panteha <panteha.ahmadi@gmail.com>
2017-06-12 17:15:47 +0100

    Added test to see if space can be viewed

---
+++ spec/feature/space_spec.rb
@@ -0,0 +1,11 @@
+describe 'Space' do
+
+  it 'A space is shown on the list' do
+    Space.create(name: 'Room1', description: '2 x 2', price: '30')
+    visit('/spaces')
+    expect(page.status_code).to eq 200
+    click_button('Show list of spaces')
+    expect(page).to have_content('Room1' && '2 x 2' && '30')
+  end
+
+end
