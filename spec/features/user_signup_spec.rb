b8fc245449121d982fbb7682cbdc1c7804b34224
Panteha <panteha.ahmadi@gmail.com>
2017-06-12 20:16:54 +0100

    Deleted extra feature folder

--- spec/features/user_signup_spec.rb
+++
@@ -1,9 +0,0 @@
-feature 'creating a user' do
-
-  scenario 'user visits /new and receives a form to create a peep' do
-    visit '/signup'
-    click_button('Submit')
-    expect(page).to have_content('YO YO YO')
-  end
-
-end
