45c40ec5762cd425066397f62a6758bdc30d0eb4
Panteha <panteha.ahmadi@gmail.com>
2017-06-14 22:46:31 +0100

    Added class method booked? to booking model

--- app/controllers/booking.rb
+++ app/controllers/booking.rb
@@ -10,15 +10,7 @@ class Makersbnb < Sinatra::Base
     book_from = Date.parse(params[:book_from])
     book_to = Date.parse(params[:book_to])
     raise 'Chosen date is not available' if !space.available?(book_from, book_to)
-    cur_date = book_from
-    while cur_date <= book_to do
-      count = Booking.count(:date => cur_date, :space => space)
-      if count != 0 then
-        raise "#{cur_date} is already booked (#{count})"
-      end
-      puts "count for #{cur_date} is #{count}"
-      cur_date += 1
-    end
+    raise 'Is already booked' if Booking.booked?(book_from, book_to, space)
     cur_date = book_from
     while cur_date <= book_to do
       Booking.create(:date => cur_date, :space => space)
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,4 +8,18 @@ class Booking
   property :date, DateTime, :required => true
 
   belongs_to :space
+
+
+  def self.booked?(book_from, book_to, space)
+    cur_date = book_from
+    while cur_date <= book_to do
+      count = Booking.count(:date => cur_date, :space => space)
+      return true if count != 0
+      cur_date += 1
+    end
+    return false
+  end
+
+
+
 end
