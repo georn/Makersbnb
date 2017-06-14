e9639f55b6de2d60fd577bd2e2dfd619cbc42c90
jade <jadealvares@hotmail.co.uk>
2017-06-14 16:41:29 +0100

    created bookings controller and modified the booking views and spaces views

--- app/controllers/booking.rb
+++ app/controllers/booking.rb
@@ -2,10 +2,15 @@ class Makersbnb < Sinatra::Base
 
 get '/booking/new' do
   @space_id = params[:space_id]
+
   erb :'bookings/new'
 end
 
 post '/booking/new' do
-  "#{params[:space_id]}, #{params[:available_from]} #{params[:available_to]}"
+  space = Space.get(params[:space_id])
+  p "yo #{space.available_from}"
+  p "work for meeee #{space.available_to}"
+  # Booking.create(date: params[:date])
+  # redirect '/spaces'
 end
 end
