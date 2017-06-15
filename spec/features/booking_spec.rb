3bb06733bfb3354f4ea6047faba823e677c658cf
Guillermo <mrog0@outlook.com>
2017-06-15 11:02:28 +0100

    Minor refactoring for Booking spaces

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -1,8 +1,11 @@
-feature 'booking' do
+feature 'Booking' do
 
   scenario 'user can make a booking request' do
     Space.create(name: 'Room1', description: '2 x 2', price: 30, available_from: '12-01-18', available_to: '13-01-18')
-    booking_a_space
+    initialize_booking
+    fill_in('book_from', with: '12-01-18')
+    fill_in('book_to', with: '13-01-18')
+    click_button('Make a booking')
     expect(page).to have_content('Booked.')
   end
 
@@ -16,7 +19,10 @@ feature 'booking' do
   scenario 'User can not choose outside the available dates for rent' do
     Space.create(name: 'Room1', description: '2 x 2', price: 30, available_from: '12-01-18', available_to: '13-01-18')
     booking_a_space
-    booking_the_space
+    initialize_booking
+    fill_in('book_from', with: '11-01-18')
+    fill_in('book_to', with: '13-01-18')
+    click_button('Make a booking')
     expect(page).to have_content('Chosen date is not available')
   end
 
