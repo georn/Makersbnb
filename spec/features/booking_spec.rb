ab9dcbb0f8a3a87f512859095e61d04ac5fc5d0f
Panteha <panteha.ahmadi@gmail.com>
2017-06-15 09:10:26 +0100

    Used booking_a_space method in first test of booking

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -2,12 +2,7 @@ feature 'booking' do
 
   scenario 'user can make a booking request' do
     Space.create(name: 'Room1', description: '2 x 2', price: 30, available_from: '12-01-18', available_to: '13-01-18')
-    visit('/spaces')
-    click_button('Show list of spaces')
-    click_button('Book')
-    fill_in('book_from', with: '12-01-18')
-    fill_in('book_to', with: '13-01-18')
-    click_button('Make a booking')
+    booking_a_space
     expect(page).to have_content('Booked.')
   end
 
